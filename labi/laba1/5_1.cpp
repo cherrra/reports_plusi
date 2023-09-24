ed7637e7b20a91b32a1807ae4ddcc1deba3ff508
cherrra <94289157+cherrra@users.noreply.github.com>
2023-09-24 14:39:57 +0300

    commit

--- labi/laba1/5_1.cpp
+++ labi/laba1/5_1.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 int main() {
-    double s=0, x = -15.246, y = 4.642e-2, z = 21;
-    s = log(pow(y, abs(sqrt(-x)))) * (x - (y/2)) + pow(sin(atan(z)), 2);
-cout<< "S = "<<s;
+    double s=0, a=0, b=0, x=-15.246, y=4.642e-2, z=21;
+    a=log(pow(y, -sqrt(abs(x))))*(x-(y/2));
+    b=pow(sin(atan(z)),2);
+    s=a+b;
+    cout<<"S="<<s;
     return 0;
 }
