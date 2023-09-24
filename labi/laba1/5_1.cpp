65169e342174d3c3504748ae409cf0c7da79bd77
cherrra <94289157+cherrra@users.noreply.github.com>
2023-09-24 14:11:18 +0300

    commit

---
+++ labi/laba1/5_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double s=0, x = -15.246, y = 4.642e-2, z = 21;
+    s = log(pow(y, abs(sqrt(-x)))) * (x - (y/2)) + pow(sin(atan(z)), 2);
+cout<< "S = "<<s;
+    return 0;
+}
