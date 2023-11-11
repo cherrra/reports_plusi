523c46fb51922c53586f988c76a5f1f4beb8fa7f
cherrra <94289157+cherrra@users.noreply.github.com>
2023-11-11 13:39:58 +0300

    commit

---
+++ labi/laba4/1_4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countSentences(const string& text) {
+    int count = 0;
+    for (char c : text) {
+        if (c == '.' || c == '!' || c == '?') {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    string text1 = "хочу домой.";
+    string text2 = "когда домой? вот бы поспать!";
+
+    int sentences1 = countSentences(text1);
+    int sentences2 = countSentences(text2);
+
+    cout << "Кол-во предложений в 1 тексте: " << sentences1 << endl;
+    cout << "Кол-во предложений во 2 тексте: " << sentences2 << endl;
+
+    return 0;
+}
