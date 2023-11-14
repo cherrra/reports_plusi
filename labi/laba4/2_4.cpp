8b3ee751eaa3c29436a9820daf8687995c561907
cherrra <94289157+cherrra@users.noreply.github.com>
2023-11-14 17:41:37 +0300

    commit

--- labi/laba4/2_4.cpp
+++ labi/laba4/2_4.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-
 int main() {
-    string word1 = "cat";
-    string word2 = "apple";
-    string word3 = "dog";
+    string word1, word2, word3;
+    cout << "Введите первое слово: ";
+    cin >> word1;
+    cout << "Введите второе слово: ";
+    cin >> word2;
+    cout << "Введите третье слово: ";
+    cin >> word3;
 
     string result = "";
     result += word1[word1.length() - 1];
