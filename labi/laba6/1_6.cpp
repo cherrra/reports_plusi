f66f5cfd27a0756483d42b196bd56e8a33d9821e
cherrra <94289157+cherrra@users.noreply.github.com>
2023-11-21 13:56:51 +0300

    c

---
+++ labi/laba6/1_6.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cout << "Введите количество строк: ";
+    cin >> n;
+    cout << "Введите количество столбцов: ";
+    cin >> m;
+
+   
+    int **a = new int*[n];
+    for (int i = 0; i < n; i++) {
+        a[i] = new int[m];
+    }
+
+    cout << "Введите элементы матрицы:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    //создание одномерного массива для хранения произведений 
+    int *proizvedenie = new int[m];
+    for (int j = 0; j < m; j++) {
+        proizvedenie[j] = 1; 
+    }
+
+    //вычисление произведений
+    for (int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++) {
+            if (a[i][j] > 0) {
+                proizvedenie[j] *= a[i][j];
+            }
+        }
+    }
+
+    cout << "Произведения положительных элементов столбцов:" << endl;
+    for (int j = 0; j < m; j++) {
+        cout << "Столбец " << j + 1 << ": " << proizvedenie[j] << endl;
+    }
+
+    for (int i = 0; i < n; i++) {
+        delete [] a[i];
+    }
+    
+    delete [] a;
+    delete [] proizvedenie;
+
+    return 0;
+}
